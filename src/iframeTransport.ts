745e8ef3113a953ee92d584501f4530052b29da4
unadlib <unadlib@gmail.com>
2020-09-05 11:33:36 +0800

    feat(iframetransport): add `iframeTransport` API

---
+++ src/iframeTransport.ts
@@ -0,0 +1,55 @@
+import { ListenOptions, TransportDataMap, TransportOptions } from './interface';
+import { Transport } from './transport';
+
+export interface IFrameTransportOptions extends Partial<TransportOptions> {
+  targetOrigin?: string;
+}
+
+export interface IFrameExternalTransportOptions extends IFrameTransportOptions {
+  iframe: HTMLIFrameElement;
+}
+
+export class IFrameExternalTransport<
+  T extends TransportDataMap = any
+> extends Transport<T> {
+  constructor({
+    iframe,
+    targetOrigin = '*',
+    listen = (callback) => {
+      window.addEventListener('message', ({ data }: MessageEvent<any>) =>
+        callback(data)
+      );
+    },
+    send = (message: any) =>
+      iframe.contentWindow!.postMessage(message, targetOrigin),
+  }: IFrameExternalTransportOptions) {
+    super({
+      listen,
+      send,
+    });
+  }
+}
+
+export class IFrameInternalTransport<
+  T extends TransportDataMap = any
+> extends Transport<T> {
+  constructor({
+    targetOrigin = '*',
+    listen = (callback: (options: ListenOptions) => void) => {
+      window.addEventListener('message', ({ data }: MessageEvent<any>) =>
+        callback(data)
+      );
+    },
+    send = (message: any) => window.parent.postMessage(message, targetOrigin),
+  }: IFrameTransportOptions) {
+    super({
+      listen,
+      send,
+    });
+  }
+}
+
+export const IFrameTransport = {
+  External: IFrameExternalTransport,
+  Internal: IFrameInternalTransport,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './decorators';
 export * from './transport';
+export * from './iframeTransport';
 export * from './interface';
