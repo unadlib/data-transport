dea39d174dcfbc274802a48b03ee8c28bb5c9015
unadlib <unadlib@gmail.com>
2020-09-10 23:11:39 +0800

    feat(broadcast): adjust broadcast interface

--- src/transports/broadcastTransport.ts
+++ src/transports/broadcastTransport.ts
@@ -1,15 +1,25 @@
 import { TransportDataMap, TransportOptions } from '../interface';
 import { Transport } from '../transport';
 
+const defaultChannel = '$$BroadcastChannel_Transport$$';
+
 export interface BroadcastTransportOptions extends Partial<TransportOptions> {
-  broadcastChannel: BroadcastChannel;
+  /**
+   * Specify a broadcast channel name.
+   */
+  channel?: string;
+  /**
+   * Specify a broadcast channel instance.
+   */
+  broadcastChannel?: BroadcastChannel;
 }
 
 abstract class BroadcastTransport<
   T extends TransportDataMap = any
 > extends Transport<T> {
   constructor({
-    broadcastChannel,
+    channel = defaultChannel,
+    broadcastChannel = new BroadcastChannel(channel),
     listener = (callback) => {
       broadcastChannel.onmessage = ({ data }) => {
         callback(data);
